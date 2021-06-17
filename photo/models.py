326c66a0493af4aa344a873a40454c1770242943
OH <wjdppm4@gmail.com>
2021-06-17 15:36:16 +0900

    two

--- photo/models.py
+++ photo/models.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import User
 class Photo(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
     text = models.TextField(blank=True)
-    image = models.ImageField(upload_to ='timeline_photo/%Y/%m/%d')
+    image = models.ImageField(upload_to = 'timeline_photo/%Y/%m/%D')
     created = models.DateTimeField(auto_now_add=True)
-    update = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return "text :"+self.text
--- photo/views.py
+++ photo/views.py
@@ -10,14 +10,25 @@ class PhotoList(ListView):
 
 class PhotoCreate(CreateView):
     model =Photo
-    fields = ['text', 'image']
+    fields = ['author', 'text', 'image']
     template_name_suffix = '_create'
 
 class PhotoUpdate(UpdateView):
     model = Photo
-    fields = ['text', 'image']
+    fields = ['author', 'text', 'image']
     template_name_suffix = '_update'
     success_url = '/'
+
+    def form_valid(self, form):
+        form.instance.author_id = self.request.user.id
+        if form.is_valid():
+            #올바르다면
+            # form : 모델폼
+            form.instance.save()
+            return redirect('/')
+        else:
+            #올바르지 않다면
+            return self.render_to_response({'form':form})
 
 class PhotoDelete(DeleteView):
     model = Photo
