e4ce94db7dbc525c8e2abf6c9b00568a0f187df7
OH <wjdppm4@gmail.com>
2021-06-17 14:49:22 +0900

    first

---
+++ photo/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Photo(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    text = models.TextField(blank=True)
+    image = models.ImageField(upload_to ='timeline_photo/%Y/%m/%d')
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "text :"+self.text
+
+    class Meta:
+        ordering = ['-created']
---
+++ photo/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic.detail import DetailView
+from .models import Photo
+
+class PhotoList(ListView):
+    model = Photo
+    template_name_suffix = '_list'
+
+class PhotoCreate(CreateView):
+    model =Photo
+    fields = ['text', 'image']
+    template_name_suffix = '_create'
+
+class PhotoUpdate(UpdateView):
+    model = Photo
+    fields = ['text', 'image']
+    template_name_suffix = '_update'
+    success_url = '/'
+
+class PhotoDelete(DeleteView):
+    model = Photo
+    template_name_suffix = '_delete'
+    success_url = '/'
+
+class PhotoDetail(DetailView):
+    model = Photo
+    template_name_suffix = '_detail'
